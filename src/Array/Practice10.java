7c61b26035d5bfca88d160adfbc1d702887485d3
zhangxue <zhangxue@kayak.com.cn>
2019-10-27 17:52:43 +0800

    算法练习10、11

---
+++ src/Array/Practice10.java
@@ -0,0 +1,111 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practice10 {
+    /**
+     * 题目：有效的数独
+     * <p>
+     * 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+     * 数字 1-9 在每一行只能出现一次。
+     * 数字 1-9 在每一列只能出现一次。
+     * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+     * <p>
+     * 题目地址：
+     * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/30/
+     */
+    public static void main(String[] args) {
+        char[][] array = {
+                {'.', '8', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        System.out.println(isValidSudoku(array));
+    }
+
+    public static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            int index = 0;
+            char[] array = new char[9];
+            char[] array1 = new char[9];
+
+            for (int j = 0; j < 9; j++) {
+                array[index] = board[i][j];
+                array1[index] = board[j][i];
+                index++;
+            }
+            if (!check(array)) {
+                return false;
+            }
+            if (!check(array1)) {
+                return false;
+            }
+        }
+
+        char[] array3 = new char[9];
+        for (int row = 0; row < 9; row += 3) {
+            for (int column = 0; column < 9; column += 3) {
+                int index = 0;
+                for (int i = row; i < row + 3; i++) {
+                    for (int j = column; j < column + 3; j++) {
+                        array3[index++] = board[i][j];
+                    }
+                }
+                if (!check(array3)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean check(char[] array) {
+        Map<Character, Character> map = new HashMap<>();
+        for (char c : array) {
+            if (c == '.') {
+                continue;
+            }
+            if (map.containsKey(c)) {
+                return false;
+            }
+            map.put(c, c);
+        }
+        return true;
+    }
+
+    /**
+     * 别人的方法
+     *
+     * @param board
+     * @return
+     */
+    public boolean isValidSudoku0(char[][] board) {
+        int[] rows = new int[9];
+        int[] columns = new int[9];
+        int[] boxes = new int[9];
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                char c = board[row][column];
+                if (c == '.') {
+                    continue;
+                }
+                int val = 1 << c - '1';
+                int i = row / 3 * 3 + column / 3;
+                if (((rows[row] | columns[column] | boxes[i]) & val) > 0) {
+                    return false;
+                }
+                rows[row] |= val;
+                columns[column] |= val;
+                boxes[i] |= val;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/Array/Practice11.java
@@ -0,0 +1,82 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice11 {
+    /**
+     * 题目：旋转图像
+     * 给定一个 n × n 的二维矩阵表示一个图像。
+     * 将图像顺时针旋转 90 度。
+     * <p>
+     * 说明：
+     * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+     * <p>
+     * 题目地址：
+     * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/31/
+     */
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+//        System.out.println(matrix.length);
+        rotate(matrix);
+        for (int[] ma : matrix) {
+            for (int m : ma) {
+                System.out.print(m + ",");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotate(int[][] matrix) {
+        int length = matrix.length;
+        //旋转次数
+        int count;
+        if (length % 2 != 0) {
+            count = (length - 1) / 2;
+        } else {
+            count = length / 2;
+        }
+        for (int i = 0; i < count; i++) {
+            List<Integer> list = new ArrayList<>();
+            //length - 2i :旋转层的阶数
+            int step = length - 2 * i;
+            for (int j = 0; j < step - 1; j++) {
+                //保存水平行数据，将剩余行列进行旋转
+//                list.add(matrix[i][j]);
+                int temp = matrix[i][j + i];
+                //将④复制给①
+                matrix[i][j + i] = matrix[step - j + i - 1][i];
+                //将③赋值给④
+                matrix[step - j + i - 1][i] = matrix[step - 1 + i][step - j + i - 1];
+                //将②赋值给③
+                matrix[step - 1 + i][step - j + i - 1] = matrix[j + i][step - 1 + i];
+                //将①赋值给②
+                matrix[j + i][step - 1 + i] = temp;
+            }
+        }
+    }
+
+    public void rotate0(int[][] matrix) {
+        int length = matrix.length;
+        //沿对角线交换
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        //沿对称轴交换
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length / 2; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[i][length - j - 1];
+                matrix[i][length - j - 1] = t;
+            }
+        }
+    }
+
+}
