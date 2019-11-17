8fa91de1d66589c20f570e0da65a5ec21e5f5143
zhangxue <zhangxue@kayak.com.cn>
2019-11-17 16:45:46 +0800

    链表练习1

---
+++ src/List/Practice1.java
@@ -0,0 +1,48 @@
+package List;
+
+public class Practice1 {
+    /**
+     * 题目：删除链表中的节点
+     * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+     * <p>
+     * 示例
+     * 输入: head = [4,5,1,9], node = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+     * <p>
+     * 题目地址：
+     * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/41/
+     */
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        deleteNode(head.next);
+
+        printNode(head);
+    }
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    private static void printNode(ListNode node) {
+        while (node.next != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.print(node.val);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
