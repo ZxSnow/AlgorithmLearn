6608cf9c48bd2bb79bc5d55159f2c79fff6880e7
ZxSnow <852322813@qq.com>
2020-10-08 22:48:41 +0800

    动态规划2、3题

---
+++ src/DynamicProgram/Practice2.java
@@ -0,0 +1,18 @@
+package DynamicProgram;
+
+public class Practice2 {
+    /**
+     * 买卖股票（只交易一次）
+     * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn8fsh/
+     */
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int res = 0;
+        int minBuy = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            minBuy = Math.min(minBuy, prices[i]);
+            res = Math.max(res, prices[i] - minBuy);
+        }
+        return res;
+    }
+}
---
+++ src/DynamicProgram/Practice3.java
@@ -0,0 +1,28 @@
+package DynamicProgram;
+
+import java.util.Arrays;
+
+public class Practice3 {
+    /**
+     * 最大子序和
+     * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn3cg3/
+     * 用nums[] 来表示了dp[]
+     * dp[i]的含义表示为 以第i位元素结尾的最大子序列和
+     * dp[i]的初始化为 元素本身
+     * 转移的条件，因为最大子序的连续性，因此在计算dp[i]时，判断dp[i-1]是否>0,只有大于0时才把它们连接起来
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > 0) {
+                nums[i] += nums[i - 1];
+            }
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+}
