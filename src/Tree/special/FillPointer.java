b4ea9b52329f2fbdccdcc4dcd60a0b6d79710950
icecloa <852322813@qq.com>
2020-10-21 21:53:27 +0800

    二叉树专题

--- src/Tree/special/FillPointer.java
+++ src/Tree/special/FillPointer.java
@@ -1,5 +1,12 @@
 package Tree.special;
 
+import Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class FillPointer {
     public Node connect(Node root) {
         help(root.left, root.right);
@@ -7,7 +14,7 @@ public class FillPointer {
     }
 
     public void help(Node left, Node right) {
-        if (left == null||right==null) {
+        if (left == null || right == null) {
             return;
         }
         left.next = right;
@@ -21,8 +28,86 @@ public class FillPointer {
         Node no = new Node(1);
         no.left = new Node(2);
         no.right = new Node(3);
-        fp.connect(no);
+        no.left.left = new Node(4);
+        no.left.right = new Node(5);
+        no.right.right = new Node(7);
+//        no.left.left.left = new Node(7);
+//        no.right.right.right = new Node(8);
+        fp.connectV3(no);
     }
+
+    /**
+     * 用层级遍历的想法，超出了内存限制
+     *
+     * @param root
+     */
+    public Node connectV2(Node root) {
+        if (root == null) return null;
+
+        List<Node> nodes = new ArrayList<>();
+        if (root.left != null) nodes.add(root.left);
+        if (root.right != null) nodes.add(root.right);
+
+        while (nodes.size() > 0) {
+            List<Node> list = new ArrayList<>();
+            for (int i = 0; i < nodes.size() - 1; i++) {
+                Node before = nodes.get(i);
+                Node next = nodes.get(i + 1);
+                before.next = next;
+                if (before.left != null) {
+                    list.add(before.left);
+                }
+                if (before.right != null) {
+                    list.add(before.right);
+                }
+                if (next.left != null) {
+                    list.add(next.left);
+                }
+                if (next.right != null) {
+                    list.add(next.right);
+                }
+            }
+            nodes = list;
+        }
+        return root;
+    }
+
+    /**
+     * 广度优先搜索（待完善） 超出限制
+     *
+     * @param root
+     * @return
+     */
+    public Node connectV3(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while (queue.size() > 1) {
+            int size = queue.size();
+            for (int i = 0; i < size - 1; i++) {
+                Node before = queue.poll();
+                Node next = queue.peek();
+                before.next = next;
+                if (before.left != null) {
+                    queue.add(before.left);
+                }
+                if (before.right != null) {
+                    queue.add(before.right);
+                }
+                if (next.left != null) {
+                    queue.add(next.left);
+                }
+                if (next.right != null) {
+                    queue.add(next.right);
+                }
+                if (i == size - 2) {
+                    queue.poll();
+                }
+            }
+        }
+        return root;
+    }
+
 }
 
 class Node {
