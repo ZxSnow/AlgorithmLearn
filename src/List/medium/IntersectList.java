2660d31d6c1ba62c320e5e0d17fcf0d94f5b246a
icecloa <852322813@qq.com>
2020-11-02 18:33:59 +0800

    链表题目三

---
+++ src/List/medium/IntersectList.java
@@ -0,0 +1,50 @@
+package List.medium;
+
+import List.ListNode;
+
+public class IntersectList {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int aLen = length(headA);
+        int bLen = length(headB);
+
+        int gap = aLen - bLen;
+        gap = gap > 0 ? gap : -gap;
+        ListNode max = aLen > bLen ? headA : headB;
+        ListNode min = aLen > bLen ? headB : headA;
+
+        while (gap > 0) {
+            max = max.next;
+            gap--;
+        }
+        while (max!=null&&min!=null){
+            if(max.equals(min)){
+                return max;
+            }
+            max = max.next;
+            min = min.next;
+        }
+        return null;
+    }
+
+    public int length(ListNode node) {
+        int res = 0;
+        while (node != null) {
+            res++;
+            node = node.next;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        IntersectList il = new IntersectList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        il.getIntersectionNode(head,head2);
+    }
+}
