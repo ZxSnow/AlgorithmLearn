ac275e3714f426cd9cf0516dafa07cc14bfb6a29
icecloa <852322813@qq.com>
2021-04-24 13:42:26 +0800

    HotTop100(14/100)

--- src/HotTop100/MaxArea.java
+++ src/HotTop100/MaxArea.java
@@ -1,13 +1,37 @@
 package HotTop100;
 
 public class MaxArea {
-    public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int max = Math.min(height[left], height[right]) * (right - left);
-        for (int i = left; i < right; i++) {
 
+    public int maxAreaBase(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int area = (j - i) * Math.min(height[i], height[j]);
+                max = Math.max(max, area);
+            }
         }
         return max;
+    }
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        MaxArea ma = new MaxArea();
+        int res = ma.maxArea(height);
+        System.out.println(res);
     }
 }
